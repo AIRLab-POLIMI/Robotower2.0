8983444547f7a08d239d0e9ebbbd8a3206f4f4b5
Stefano Boriero <stefano.boriero@gmail.com>
2019-03-28 17:31:04 +0100

    Sketch for player tracking with optitrack

---
+++ perception/player_tracking/src/particle_filter/player_ground_truth.cpp
@@ -0,0 +1,53 @@
+#include <ros/ros.h>
+#include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/Pose2D.h>
+
+class PlayerPositionRepublisher{
+
+    private:
+        ros::NodeHandle nh_;
+        ros::Subscriber optitrackSubscriber_;
+        ros::Publisher playerPositionPublisher_; 
+        geometry_msgs::Pose2D position2D_;
+
+        void optitrackCallback(geometry_msgs::Pose2D position2D){
+            position2D_ = position2D;
+        }
+
+
+        
+    public:
+
+    PlayerPositionRepublisher(){
+        optitrackSubscriber_ = nh_.subscribe("/player/ground_pose", 1, &PlayerPositionRepublisher::optitrackCallback, this);
+        playerPositionPublisher_ = nh_.advertise<geometry_msgs::PointStamped>("/player_ground_truth", 1000);
+
+    }
+
+    void publishPosition(){
+         geometry_msgs::PointStamped playerPosition;
+            playerPosition.header.stamp = ros::Time::now();
+            playerPosition.header.frame_id = "/map";
+            playerPosition.point.y = (position2D_.x + 1.615);
+            playerPosition.point.x = -(position2D_.y - 1.72);
+            playerPositionPublisher_.publish(playerPosition);
+    }
+        
+
+};
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "player_ground_truth");
+
+	/** @todo We need to get a param, scan_topic, which is needed for the initialization
+	list of OccupancyGridMapping. Is there a clearer way of doing this? */
+	ros::NodeHandle nh;
+    ros::Rate r(20);
+    PlayerPositionRepublisher repub;
+    while(ros::ok()){
+        repub.publishPosition();
+        ros::spinOnce();
+        r.sleep();
+    }
+	return 0;
+}
